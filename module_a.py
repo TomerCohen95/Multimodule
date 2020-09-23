44a9b0fcd3688d787eb689cc9222feb6048b1194
Tomer Cohen QA <tomerco@deepinstinct.com>
2020-09-23 09:35:01 +0300

    modules

--- module_a.py
+++ module_a.py
@@ -1,8 +1,13 @@
-# First Project (ModuleA)
+from ModuleB.module_b import ModuleB
+
+
 class ModuleA:
-  def __init__(self, module_b: ModuleB):
-    print('im code from module A')
-    module_b.hello()
-    
-  def hello(self):
-    print('hello from A')
+    def __init__(self, module_b: ModuleB):
+        print('im code from module A')
+        module_b.hello()
+
+    def hello(self):
+        print('hello from A')
+
+
+ModuleA(ModuleB()).hello()
