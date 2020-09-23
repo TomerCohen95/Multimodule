458da24daf7c0b4d4d76e8bd54a260e7e7c9ec8d
TomerCohen95 <tomercohen95@gmail.com>
2020-09-23 09:26:59 +0300

    Update module_a.py

--- module_a.py
+++ module_a.py
@@ -1,8 +1,8 @@
 # First Project (ModuleA)
 class ModuleA:
-  def __init__(module_b: ModuleB):
+  def __init__(self, module_b: ModuleB):
     print('im code from module A')
     module_b.hello()
     
-  def hello():
+  def hello(self):
     print('hello from A')
